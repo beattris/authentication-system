7976395ac217796696182351497e918f9e8184bc
Beattris <beatriceebirim@gmail.com>
2022-03-19 15:57:34 +0100

    added auto-logout function to logout a user after a particular time

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -55,7 +55,10 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        authCtx.login(data.idToken);
+        // deriving the expiration time. The + in '+data.expiresIn' is used to change the detail to a number, then 
+        // multiplied by 1000 to change it from secs to milisecs.
+        const expirationTime = new Date(new Date().getTime() + (+data.expiresIn * 1000));
+        authCtx.login(data.idToken, expirationTime.toString());
         history.replace('/');
       })
       .catch((err) => {
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,25 +7,35 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjustedExpirationTime = new Date(expirationTime).getTime();
+  const remainingDuration = adjustedExpirationTime - currentTime;
+
+  return remainingDuration;
+};
+
 export const AuthContextProvider = (props) => {
-    // checking if there is a token already stored in the browser and
-    // using it as the initial state of this token state
-  const initialToken = localStorage.getItem('token');
+  // checking if there is a token already stored in the browser and
+  // using it as the initial state of this token state
+  const initialToken = localStorage.getItem("token");
   const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    // using browser local storage to store the token so that a
-    // user doesn't logout when the page refreshes
-    localStorage.setItem('token', token);
-  };
-
   const logoutHandler = () => {
     setToken(null);
     // removing the stored token from the browser local storage when the user logs out
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    // using browser local storage to store the token so that a
+    // user doesn't logout when the page refreshes
+    localStorage.setItem("token", token);
+    const remainingTime = calculateRemainingTime(expirationTime);
+    setTimeout(logoutHandler, remainingTime);
   };
 
   const contextValue = {
