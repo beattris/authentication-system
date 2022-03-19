f29aec7cf61857bcb06280e3ecc79c41324e5ef6
Beattris <beatriceebirim@gmail.com>
2022-03-19 15:39:31 +0100

    used localStorage to persist a user authentication status

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,16 +8,24 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+    // checking if there is a token already stored in the browser and
+    // using it as the initial state of this token state
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    // using browser local storage to store the token so that a
+    // user doesn't logout when the page refreshes
+    localStorage.setItem('token', token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    // removing the stored token from the browser local storage when the user logs out
+    localStorage.removeItem('token');
   };
 
   const contextValue = {
